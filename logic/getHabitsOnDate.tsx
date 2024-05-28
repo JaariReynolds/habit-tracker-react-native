1c005bbc22fa4868c8c733199dfeb0ee1c227cf4
Jaari <jaari.rey@gmail.com>
2024-05-28 13:18:28 +1000

    first (non-working) commit to showing day-specific habits

---
+++ logic/getHabitsOnDate.tsx
@@ -0,0 +1,47 @@
+import { CustomFrequency, Habit, WeeklyFrequency } from "../interfaces/habit";
+
+export default function getHabitsOnDate(date: Date, habits: Habit[]): Habit[] {
+  return habits.filter((habit) => {
+    if (habit.frequency == "Daily") return habit;
+
+    const castedFrequency = habit.frequency as WeeklyFrequency;
+
+    // if weekly
+    if (castedFrequency.days) {
+      if (castedFrequency.days.includes(date.getDay())) {
+        return habit;
+      }
+      // if custom
+    } else {
+      if (
+        isHabitWithinDaysMultiple(
+          date,
+          (habit.frequency as CustomFrequency).lastUpdated,
+          (habit.frequency as CustomFrequency).customFrequency
+        )
+      ) {
+        return habit;
+      }
+    }
+  });
+}
+
+// returns true if the gap in days between date1 and date2 are a multiple of daysGap apart
+function isHabitWithinDaysMultiple(
+  date1: Date,
+  date2: Date,
+  daysGap: number
+): boolean {
+  const millisecondsPerDay = 86400000;
+  const differenceInMilliseconds = Math.abs(
+    toMidnight(date1).getTime() - toMidnight(date2).getTime()
+  );
+  const differenceInDays = Math.round(
+    differenceInMilliseconds / millisecondsPerDay
+  );
+  return differenceInDays % daysGap === 0;
+}
+
+function toMidnight(date: Date): Date {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+}
