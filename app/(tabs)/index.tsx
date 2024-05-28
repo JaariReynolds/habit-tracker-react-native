07a7251c96ebb0c263549b12bda2ca76f5eee83c
Jaari <jaari.rey@gmail.com>
2024-05-28 12:22:23 +1000

    working bare example of simple carousel

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import FullPageView from "../../components/FullPageView";
 import RouterPushButton from "../../components/buttons/RouterPushButton";
@@ -8,11 +8,16 @@ import FullHeightScrollView from "../../components/FullHeightScrollView";
 import Header from "../../components/Header";
 import HabitPreview from "../../components/HabitPreview";
 import { useSwipe } from "../../hooks/useSwipe";
+import Carousel, { ICarouselInstance } from "react-native-reanimated-carousel";
+
+const numbers = [1, 2, 3, 4, 5];
 
 const Habits = () => {
   const { habits } = useHabitContext();
   const { onTouchStart, onTouchEnd } = useSwipe(onSwipeLeft, onSwipeRight);
   const [date, setDate] = useState<Date>(new Date());
+
+  const ref = React.useRef<ICarouselInstance>(null);
 
   function onSwipeLeft() {
     var newDate = new Date(date);
@@ -24,20 +29,40 @@ const Habits = () => {
     var newDate = new Date(date);
     newDate.setDate(newDate.getDate() - 1);
     setDate(newDate);
+    console.log("test");
   }
 
   return (
     <FullHeightScrollView onTouchStart={onTouchStart} onTouchEnd={onTouchEnd}>
-      <Header title="habits" />
-      <FullPageView>
-        <Text>{date.toDateString()}</Text>
-        <View style={styles.habitContainer}>
-          {habits.map((habit, index) => (
-            <HabitPreview key={index} arrayIndex={index} habit={habit} />
-          ))}
-        </View>
-        <RouterPushButton buttonText="New Habit :3" pageLink="./../NewHabit" />
-      </FullPageView>
+      <Carousel
+        ref={ref}
+        width={Dimensions.get("window").width}
+        style={{ flex: 1 }}
+        data={numbers}
+        renderItem={() => {
+          return (
+            <>
+              <Header title="habits" />
+              <FullPageView>
+                <Text>{date.toDateString()}</Text>
+                <View style={styles.habitContainer}>
+                  {habits.map((habit, index) => (
+                    <HabitPreview
+                      key={index}
+                      arrayIndex={index}
+                      habit={habit}
+                    />
+                  ))}
+                </View>
+                <RouterPushButton
+                  buttonText="New Habit :3"
+                  pageLink="./../NewHabit"
+                />
+              </FullPageView>
+            </>
+          );
+        }}
+      />
     </FullHeightScrollView>
   );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import HabitContextProvider from "../contexts/habitContext";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const RootLayout = () => {
   return (
-    <SafeAreaProvider>
-      <HabitContextProvider>
-        <SafeAreaView style={{ height: "100%" }}>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="NewHabit" options={{ headerShown: false }} />
-            <Stack.Screen name="EditHabit" options={{ headerShown: false }} />
-          </Stack>
-        </SafeAreaView>
-      </HabitContextProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <HabitContextProvider>
+          <SafeAreaView style={{ height: "100%" }}>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="NewHabit" options={{ headerShown: false }} />
+              <Stack.Screen name="EditHabit" options={{ headerShown: false }} />
+            </Stack>
+          </SafeAreaView>
+        </HabitContextProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 };
 
