0de65da243e71c0dc32622a4b10b7b98b80abf3a
Jaari <jaari.rey@gmail.com>
2024-05-29 12:11:56 +1000

    carousel works on button press, vertical scroll still working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,8 +9,10 @@ import Header from "../../components/Header";
 import HabitPreview from "../../components/HabitPreview";
 import { useSwipe } from "../../hooks/useSwipe";
 import Carousel, { ICarouselInstance } from "react-native-reanimated-carousel";
+import { interpolate } from "react-native-reanimated";
 
 const numbers = [1, 2, 3, 4, 5];
+const PAGE_WIDTH = Dimensions.get("window").width;
 
 const Habits = () => {
   const { habits } = useHabitContext();
@@ -32,38 +34,65 @@ const Habits = () => {
     console.log("test");
   }
 
+  function buttonPress() {
+    ref.current!.prev();
+  }
+
+  const animationStyle = React.useCallback((value: number) => {
+    "worklet";
+
+    const zIndex = interpolate(value, [-1, 0, 1], [-1000, 0, 1000]);
+    const translateX = interpolate(
+      value,
+      [-1, 0, 1],
+      [-PAGE_WIDTH, 0, PAGE_WIDTH]
+    );
+
+    return {
+      transform: [{ translateX }],
+      zIndex,
+    };
+  }, []);
+
   return (
-    <FullHeightScrollView onTouchStart={onTouchStart} onTouchEnd={onTouchEnd}>
-      <Carousel
-        ref={ref}
-        width={Dimensions.get("window").width}
-        style={{ flex: 1 }}
-        data={numbers}
-        renderItem={() => {
-          return (
-            <>
-              <Header title="habits" />
-              <FullPageView>
-                <Text>{date.toDateString()}</Text>
-                <View style={styles.habitContainer}>
-                  {habits.map((habit, index) => (
-                    <HabitPreview
-                      key={index}
-                      arrayIndex={index}
-                      habit={habit}
-                    />
-                  ))}
-                </View>
-                <RouterPushButton
-                  buttonText="New Habit :3"
-                  pageLink="./../NewHabit"
-                />
-              </FullPageView>
-            </>
-          );
-        }}
-      />
-    </FullHeightScrollView>
+    <Carousel
+      ref={ref}
+      width={PAGE_WIDTH}
+      style={{ flex: 1 }}
+      data={numbers}
+      customAnimation={animationStyle}
+      enabled={false}
+      renderItem={() => {
+        return (
+          <FullHeightScrollView
+            onTouchStart={onTouchStart}
+            onTouchEnd={onTouchEnd}
+          >
+            <Header title="habits" />
+            <FullPageView>
+              <Text>{date.toDateString()}</Text>
+              <View>
+                <Pressable
+                  onPress={buttonPress}
+                  style={{ height: 30, width: 40, backgroundColor: "blue" }}
+                >
+                  <Text>Next</Text>
+                </Pressable>
+              </View>
+              <View style={styles.habitContainer}>
+                {habits.map((habit, index) => (
+                  <HabitPreview key={index} arrayIndex={index} habit={habit} />
+                ))}
+              </View>
+              <RouterPushButton
+                buttonText="New Habit :3"
+                pageLink="./../NewHabit"
+              />
+            </FullPageView>
+          </FullHeightScrollView>
+        );
+      }}
+    />
   );
 };
 
