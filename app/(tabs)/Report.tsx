9bdab84e39da9d772941b13babf55ef0d793438b
Jaari <jaari.rey@gmail.com>
2024-06-01 00:18:15 +1000

    added testing ground for stats info

--- app/(tabs)/Report.tsx
+++ app/(tabs)/Report.tsx
@@ -1,11 +1,29 @@
 import { View, Text } from "react-native";
 import React from "react";
 import FullPageView from "../../components/FullPageView";
+import { useHabitContext } from "../../contexts/habitContext";
+import getHabitCompletion from "../../logic/reportLogic/getHabitCompletion";
 
 const HabitReports = () => {
+  const { habits } = useHabitContext();
+  const tenth = new Date(2024, 5, 10);
+  const completionArray = habits.map((habit) => {
+    return {
+      habitName: habit.habitName,
+      completion: getHabitCompletion(habit, new Date(2024, 5, 1), tenth),
+    };
+  });
   return (
     <FullPageView>
-      <Text>HabitReports</Text>
+      <View>
+        {completionArray.map((habit, index) => (
+          <View key={index}>
+            <Text>{tenth.toLocaleDateString("en-GB")}</Text>
+            <Text>name: {habit.habitName}</Text>
+            <Text>percentage: {habit.completion}</Text>
+          </View>
+        ))}
+      </View>
     </FullPageView>
   );
 };
