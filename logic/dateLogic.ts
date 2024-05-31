f4d38374d173b6b1fd8cfc20c5b9ebcbb72b86ba
Jaari <jaari.rey@gmail.com>
2024-05-31 22:53:08 +1000

    moved reusable function out

--- logic/dateLogic.ts
+++ logic/dateLogic.ts
@@ -1,7 +1,18 @@
+const MILLISECONDS_PER_DAY = 86400000;
+
 export function toMidnight(date: Date): Date {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate());
 }
 
 export function isTodaysDate(date: Date): boolean {
   return date.toLocaleDateString() == Date.now().toLocaleString();
+}
+
+// returns true if the gap in days between date1 and date2 are a multiple of daysGap apart
+export function isDateWithinDaysMultiple(date1: Date, date2: Date, daysGap: number): boolean {
+  const differenceInMilliseconds = Math.abs(
+    toMidnight(date1).getTime() - toMidnight(date2).getTime()
+  );
+  const differenceInDays = Math.round(differenceInMilliseconds / MILLISECONDS_PER_DAY);
+  return differenceInDays % daysGap === 0;
 }
--- logic/getHabitsOnDate.tsx
+++ logic/getHabitsOnDate.tsx
@@ -1,45 +1,35 @@
 import { CustomFrequency, Habit, WeeklyFrequency } from "../interfaces/habit";
-import { toMidnight } from "./dateLogic";
-
-const MILLISECONDS_PER_DAY = 86400000;
+import { isDateWithinDaysMultiple } from "./dateLogic";
 
 export default function getHabitsOnDate(date: Date, habits: Habit[]): Habit[] {
   return habits.filter((habit) => {
-    if (habit.frequency == "Daily") return habit;
-
-    const castedFrequency = habit.frequency as WeeklyFrequency;
-
-    // if weekly
-    if (castedFrequency.days) {
-      if (castedFrequency.days.includes(date.getDay())) {
-        return habit;
-      }
-      // if custom
-    } else {
-      if (
-        isHabitWithinDaysMultiple(
-          date,
-          (habit.frequency as CustomFrequency).startDate,
-          (habit.frequency as CustomFrequency).customFrequency
-        )
-      ) {
-        return habit;
-      }
-    }
+    if (isHabitOnDate(habit, date)) return habit;
   });
 }
 
-// returns true if the gap in days between date1 and date2 are a multiple of daysGap apart
-function isHabitWithinDaysMultiple(
-  date1: Date,
-  date2: Date,
-  daysGap: number
-): boolean {
-  const differenceInMilliseconds = Math.abs(
-    toMidnight(date1).getTime() - toMidnight(date2).getTime()
-  );
-  const differenceInDays = Math.round(
-    differenceInMilliseconds / MILLISECONDS_PER_DAY
-  );
-  return differenceInDays % daysGap === 0;
+export function isHabitOnDate(habit: Habit, date: Date): boolean {
+  if (habit.frequency == "Daily") {
+    return true;
+  }
+
+  const castedFrequency = habit.frequency as WeeklyFrequency;
+
+  // if weekly
+  if (castedFrequency.days) {
+    if (castedFrequency.days.includes(date.getDay())) {
+      return true;
+    }
+    // if custom
+  } else {
+    if (
+      isDateWithinDaysMultiple(
+        date,
+        (habit.frequency as CustomFrequency).startDate,
+        (habit.frequency as CustomFrequency).customFrequency
+      )
+    )
+      return true;
+  }
+
+  return false;
 }
