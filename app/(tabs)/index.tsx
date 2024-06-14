c2808d0b2fce8f683dd9785081a46f4ef9a1d9f3
Jaari <jaari.rey@gmail.com>
2024-06-14 21:03:42 +1000

    removed carousel and hard filter

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,22 +5,20 @@ import RouterPushButton from "../../components/buttons/RouterPushButton";
 import { useHabitContext } from "../../contexts/habitContext";
 import FullHeightScrollView from "../../components/FullHeightScrollView";
 import HabitPreview from "../../components/HabitPreview";
-import { ICarouselInstance } from "react-native-reanimated-carousel";
 import IndexHeader from "../../components/IndexHeader";
 
 const Habits = () => {
   const { habits } = useHabitContext();
-  const carouselRef = React.useRef<ICarouselInstance>(null);
 
   return (
     <>
-      <IndexHeader carouselRef={carouselRef} />
+      <IndexHeader />
       <FullHeightScrollView>
         <FullPageView>
           <View style={{ alignSelf: "stretch", gap: 15 }}>
-            {habits.map((habit, index) => (
-              <HabitPreview key={index} arrayIndex={index} habit={habit} />
-            ))}
+            {habits.map((habit, index) => {
+              return <HabitPreview key={index} arrayIndex={index} habit={habit} />;
+            })}
           </View>
           <RouterPushButton buttonLabel="+" pageLink="./../NewHabit" />
         </FullPageView>
