1d4e078f0e5b14d167ccd5d067f5a5195e488018
Jaari <jaari.rey@gmail.com>
2024-06-16 21:23:52 +1000

    removed unused commented code

--- hooks/animations/useOpacityAnimation.tsx
+++ hooks/animations/useOpacityAnimation.tsx
@@ -1,16 +1,12 @@
-import { useEffect, useRef } from "react";
 import { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 
 export function useOpacityAnimation(initialOpacity: number, msIn: number, msOut: number) {
   const opacity = useSharedValue(initialOpacity);
-  // const display = useSharedValue(initialOpacity === 1 ? "flex" : "none");
   const animatedOpacity = useAnimatedStyle(() => ({
     opacity: opacity.value,
-    // display: display.value as "flex" | "none" | undefined,
   }));
 
   function animateIn() {
-    // display.value = "flex";
     opacity.value = withTiming(1, { duration: msIn });
   }
 
