4e6426c66d98ccd5c9af3b67ba8b325f3b6ea0d2
Jaari <jaari.rey@gmail.com>
2024-06-22 22:57:27 +1000

    using react-navigation's tabs instead of expo-router's tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,52 +1,83 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Tabs } from "expo-router";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import {
-  IconDefinition,
-  faBook,
-  faClipboard,
-  faGamepad,
-  faHouse,
-  faStopwatch,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
+import { faChartSimple, faHouse } from "@fortawesome/free-solid-svg-icons";
 import { colours } from "../../styles/constants";
+import { BottomTabBarProps, createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Habits from ".";
+import HabitReports from "./Report";
+import { robotoFonts } from "../../styles/base-styles";
+import { StatusBar } from "expo-status-bar";
 
-const TabIcon = ({ icon, color, name }: { icon: IconDefinition; color: string; name: string }) => {
+const icons = [faHouse, faChartSimple];
+
+const Tab = createBottomTabNavigator();
+
+const CustomTabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => {
   return (
-    <View style={styles.TabIcon}>
-      <FontAwesomeIcon icon={icon} color={color} />
-      <Text style={{ color: color }}>{name}</Text>
+    <View
+      style={{
+        flexDirection: "row",
+        borderTopWidth: 1,
+        height: 43,
+      }}
+    >
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        return (
+          <View key={index} style={{ flexGrow: 1, height: "100%", justifyContent: "center" }}>
+            <Pressable
+              accessibilityRole="button"
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              style={{
+                backgroundColor: colours.light,
+                height: "100%",
+                alignItems: "center",
+              }}
+            >
+              <View style={{ alignItems: "center", justifyContent: "center", marginTop: "auto" }}>
+                <FontAwesomeIcon
+                  icon={icons[index]}
+                  size={30}
+                  color={isFocused ? colours.accent : colours.dark}
+                />
+                {/* <Text style={[robotoFonts.regular, { fontSize: 15 }]}>{route.name}</Text> */}
+              </View>
+            </Pressable>
+          </View>
+        );
+      })}
     </View>
   );
 };
 
 const TabsLayout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: colours.primary,
-        tabBarInactiveTintColor: colours.accent,
-        tabBarShowLabel: false,
-        headerShown: false,
-      }}
+    <Tab.Navigator
+      tabBar={(props) => <CustomTabBar {...props} />}
+      screenOptions={{ headerShown: false }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Habits",
-          tabBarIcon: ({ color }) => <TabIcon icon={faGamepad} color={color} name="habits" />,
-        }}
-      />
-      <Tabs.Screen
-        name="Report"
-        options={{
-          title: "report",
-          tabBarIcon: ({ color }) => <TabIcon icon={faClipboard} color={color} name="report" />,
-        }}
-      />
-    </Tabs>
+      <Tab.Screen name="Habits" component={Habits} />
+      <Tab.Screen name="Report" component={HabitReports} />
+    </Tab.Navigator>
   );
 };
 
